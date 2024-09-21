623610526b0484a88720c3889489ed868ec00ca5
myxyy <m.myxomatosis@gmail.com>
2024-09-21 23:20:14 +0900

    正解表示

---
+++ Assets/ShowPredictAnswer.cs
@@ -0,0 +1,26 @@
+﻿
+using UdonSharp;
+using UnityEngine;
+using VRC.SDKBase;
+using VRC.Udon;
+
+public class ShowPredictAnswer : UdonSharpBehaviour
+{
+    [SerializeField]
+    private IVariable _predict;
+    [SerializeField]
+    private IVariable _answer;
+    [SerializeField]
+    private Material _material;
+    private bool _isInit = false;
+
+    private void Update()
+    {
+        if (!_isInit && _predict.IsTextureReady() && _answer.IsTextureReady())
+        {
+            _material.SetTexture("_MainTex", _predict.Data());
+            _material.SetTexture("_LabelTex", _answer.Data());
+            _isInit = true;
+        }
+    }
+}
