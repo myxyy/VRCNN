39d5be4e58def0ea76a3662d0a9fca8bcdf599d9
myxyy <m.myxomatosis@gmail.com>
2024-09-29 12:26:44 +0900

    データのコピー

---
+++ Assets/CopyData.cs
@@ -0,0 +1,13 @@
+﻿
+using UdonSharp;
+using UnityEngine;
+using VRC.SDKBase;
+using VRC.Udon;
+
+public class CopyData : IVariable 
+{
+    [SerializeField]
+    private IVariable _target;
+
+    public override RenderTexture Data() => _target.Data();
+}
